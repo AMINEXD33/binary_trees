5c5c2f434a45f1281383da61fb8f0a705971747b
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-03 19:56:03 +0100

    betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	/*the parent needs to exists*/
 	if (parent == NULL)
 		return (NULL);
-	
+
 	/*node config*/
 	node->n = value;
 	node->left = NULL;
