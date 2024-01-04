58b9a64583b289ffccdfb3f8ce03a3c4baf143a9
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2024-01-04 11:12:03 -0800

    fixth the code

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,22 +8,22 @@
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+const binary_tree_t *second)
 {
-    binary_tree_t *mom, *pop;
+binary_tree_t *mom, *pop;
 
-    if (!first || !second)
-        return (NULL);
+if (!first || !second)
+return (NULL);
 
-    if (first == second)
-        return ((binary_tree_t *)first);
+if (first == second)
+return ((binary_tree_t *)first);
 
-    mom = first->parent, pop = second->parent;
+mom = first->parent, pop = second->parent;
 
-    if (first == pop || !mom || (!mom->parent && pop))
-        return (binary_trees_ancestor(first, pop));
-    else if (mom == second || !pop || (!pop->parent && mom))
-        return (binary_trees_ancestor(mom, second));
+if (first == pop || !mom || (!mom->parent && pop))
+return (binary_trees_ancestor(first, pop));
+else if (mom == second || !pop || (!pop->parent && mom))
+return (binary_trees_ancestor(mom, second));
 
-    return (binary_trees_ancestor(mom, pop));
+return (binary_trees_ancestor(mom, pop));
 }
