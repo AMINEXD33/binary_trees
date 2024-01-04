598afc89aff06eb9f2575cb47a29d35d0271a4cf
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-04 16:21:22 +0100

    fixe i guess ???!!

--- binary_trees.h
+++ binary_trees.h
@@ -12,20 +12,20 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 
 typedef struct binary_tree_s binary_tree_t;
 
-/////////////// HA CHNO NSITI MAZDTI
+
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-////////////////////////////////////
+
 
 /*##Function used##*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
